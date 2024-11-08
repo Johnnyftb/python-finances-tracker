262ae387fef76cd6b8f0667ade182762428936f7
John Pioc <johnpioc@Johns-Laptop.local>
2024-11-08 22:16:59 +1000

    created load function in Finance Tracker

--- app/FinanceTracker.py
+++ app/FinanceTracker.py
@@ -1,10 +1,31 @@
+import csv
+from datetime import datetime
+from Entry import Entry
+
+CATEGORIES = ["Rent", "Transport", "Groceries", "Other Expenses", "Credit Card", "Savings", "Wants",
+              "General Spendings", "Holiday", "Tax"]
+
 class FinanceTracker:
     def __init__(self):
         self.entries = []
+        self.categories = CATEGORIES
         return
 
     def load(self):
-        return
+        with open('../entries.csv', 'r') as csv_file:
+            for entry in csv_file:
+                date_string = entry[0]
+                date = datetime.strptime(date_string, "%d/%m/%Y")
+
+                description = entry[1]
+                category = entry[2]
+                amount = float(entry[3])
+
+                new_entry = Entry(date, description, category, amount)
+                self.entries.append(new_entry)
+
+        print(self.entries)
+
 
     def save(self):
         return
---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from FinanceTracker import FinanceTracker
+
+
+def main():
+    finance_tracker = FinanceTracker()
+    finance_tracker.load()
+
+main()
